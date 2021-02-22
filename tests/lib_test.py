b36a029fb391ac5215bb0d04f78611d9c56df727
kenza mandri <kenzamandri@gmail.com>
2021-02-22 18:18:27 +0100

    finaltry

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -1,2 +1,12 @@
+from ToolBox.lib import testing
+import pandas as pd
+
+data = pd.read_csv('raw_data/data', sep=",", header=None)
+data.columns = ['text']
+data["clean_text"] = data['text'].apply(cleaning)
+data["clean_text"]= data['clean_text'].astype('str')
+def tester():
+    text_test =['I Love football but wht I love most is baskettball where people have fun and jump']
+    assert testing(data.clean_text, text_test) == ('topic 1 :', 0.701596149051779)
 
 
